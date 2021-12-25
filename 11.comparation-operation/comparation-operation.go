c4c00c63594aacaa82f841513ac300bf5e9ee771
Eko Sugeng Cahyadi <ekosugengcahyadi@gmail.com>
2021-12-25 14:34:54 +0700

    added some golang tutorial course

---
+++ 11.comparation-operation/comparation-operation.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "Eko"
+	var name2 = "Eko"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+
+	fmt.Println(value1 > value2)
+
+}
